17996708bc2f5d18c3708409e393e2e3d0408aee
rawn01 <rohan.kaos@gmail.com>
2024-02-01 17:20:44 +0530

    place order page starting

--- frontend/src/screens/PlaceOrderScreen.tsx
+++ frontend/src/screens/PlaceOrderScreen.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import CheckoutStep from '../components/CheckoutStep';
 
 const PlaceOrderScreen = () => {
+  const navigate = useNavigate();
+  const { cartItems, shippingAddress, paymentMethod } = useAppSelector(state => state.cart);
+
+  useEffect(() => {
+    if(!shippingAddress?.address) {
+      navigate("/shipping");
+    } else if(!paymentMethod) {
+      navigate("/payment")
+    }
+  }, [shippingAddress, paymentMethod])
+
   return (
-    <div>PlaceOrderScreen</div>
+    <>
+      <CheckoutStep step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}></Col>
+        <Col md={4}></Col>
+      </Row>
+    </>
   )
 }
 
